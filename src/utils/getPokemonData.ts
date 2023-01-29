26401348dcf62f2baaf7e252500ab9c0f237e5f3
Anna Marrodán <anna.marrodan@gmail.com>
2023-01-29 12:02:43 +0100

    Modify function to obtain Pokemon data from Api to obtain an array

---
+++ src/utils/getPokemonData.ts
@@ -0,0 +1,21 @@
+import type { PokemonData } from "./types";
+
+export const getPokemonFromApi = async (id: number) => {
+  try {
+    const allPokemons = [];
+
+    for (let i = 1; i <= id; i++) {
+      const id = i;
+      const getPokemonData = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      allPokemons.push(getPokemonData);
+    }
+
+    const pokemonUnresolvedPromises = await Promise.all(allPokemons);
+    const pokemonInfo = pokemonUnresolvedPromises.map(
+      async (pokemonUnresolvedPromise) => pokemonUnresolvedPromise.json()
+    );
+    const finalPokemonInfo = (await Promise.all(pokemonInfo)) as PokemonData[];
+
+    return finalPokemonInfo;
+  } catch {}
+};
